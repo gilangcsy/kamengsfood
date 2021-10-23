a8962e534c19c59051eefa35e3f29418e01577f2
gilangcsy <gilangchandra9@gmail.com>
2021-10-23 14:00:35 +0700

    initialize

---
+++ src/scripts/index.js
@@ -0,0 +1,30 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+
+console.log('Hello Coders! :)');
+
+// const navbar = document.querySelector("#nav");
+// const navButtonOpen = document.querySelector("#navButtonOpen");
+// const navButtonClose = document.querySelector("#navButtonClose");
+
+// function openNav() {
+//     // navbar.style.transform = "translateY(0%)";
+//     navbar.classList.toggle('slide');
+//     navButtonOpen.style.display = "none";
+//     navButtonClose.style.display = "block";
+// }
+
+// function closeNav() {
+//     navbar.classList.toggle('slide');
+//     // navbar.style.transform = "translateY(-100%)";
+//     navButtonOpen.style.display = "block";
+//     navButtonClose.style.display = "none";
+// }
+
+// navButtonOpen.addEventListener('click', function () {
+//     openNav();
+// });
+
+// navButtonClose.addEventListener('click', function () {
+//     closeNav();
+// });
