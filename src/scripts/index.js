ea7b9f7f1ed7774d637cb63d4fbf33b8417ce44f
gilangcsy <gilangchandra9@gmail.com>
2021-10-23 14:16:10 +0700

    making navigation bar and sidebar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,28 +3,28 @@ import '../styles/main.css';
 
 console.log('Hello Coders! :)');
 
-// const navbar = document.querySelector("#nav");
-// const navButtonOpen = document.querySelector("#navButtonOpen");
-// const navButtonClose = document.querySelector("#navButtonClose");
+const navbar = document.querySelector("#nav");
+const navButtonOpen = document.querySelector("#navButtonOpen");
+const navButtonClose = document.querySelector("#navButtonClose");
 
-// function openNav() {
-//     // navbar.style.transform = "translateY(0%)";
-//     navbar.classList.toggle('slide');
-//     navButtonOpen.style.display = "none";
-//     navButtonClose.style.display = "block";
-// }
+function openNav() {
+    // navbar.style.transform = "translateY(0%)";
+    navbar.classList.toggle('slide');
+    navButtonOpen.style.display = "none";
+    navButtonClose.style.display = "block";
+}
 
-// function closeNav() {
-//     navbar.classList.toggle('slide');
-//     // navbar.style.transform = "translateY(-100%)";
-//     navButtonOpen.style.display = "block";
-//     navButtonClose.style.display = "none";
-// }
+function closeNav() {
+    navbar.classList.toggle('slide');
+    // navbar.style.transform = "translateY(-100%)";
+    navButtonOpen.style.display = "block";
+    navButtonClose.style.display = "none";
+}
 
-// navButtonOpen.addEventListener('click', function () {
-//     openNav();
-// });
+navButtonOpen.addEventListener('click', function () {
+    openNav();
+});
 
-// navButtonClose.addEventListener('click', function () {
-//     closeNav();
-// });
+navButtonClose.addEventListener('click', function () {
+    closeNav();
+});
